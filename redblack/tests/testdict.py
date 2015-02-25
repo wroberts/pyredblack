88014d0abd9e4552157f3b0e7d647f98e7f2e4f2
Will Roberts <will.roberts@rz.hu-berlin.de>
2015-02-25 16:55:20 +0100

    more unit tests into testdict.py

--- redblack/tests/testdict.py
+++ redblack/tests/testdict.py
@@ -8,6 +8,7 @@ testdict.py
 Unit tests for the redblack.dict class.
 '''
 
+import random
 import unittest
 from .. import redblack
 
@@ -52,3 +53,57 @@ class TestDict(unittest.TestCase):
         self.assertEqual(sorted(d.values()), [1, 2, 3])
         self.assertEqual(sorted(d.items()),
                          [('one', 3), ('three', 1), ('two', 2)])
+
+    def test_bool(self):
+        d = redblack.dict()
+        self.assertFalse(bool(d))
+        d[2] = 'something'
+        self.assertTrue(bool(d))
+
+    def test_len(self):
+        d = redblack.dict()
+        self.assertEqual(len(d), 0)
+        d[2] = 'something'
+        self.assertEqual(len(d), 1)
+        del d[2]
+        self.assertEqual(len(d), 0)
+
+    def test_missingkey(self):
+        d = redblack.dict()
+        self.assertRaises(KeyError, d.__getitem__, (2,))
+        self.assertRaises(KeyError, d.__delitem__, (2,))
+
+    def test_set(self):
+        for _try in range(15):
+            d1 = dict()
+            d2 = redblack.dict()
+            for _iter in range(5000):
+                num = random.randint(0, 10000)
+                key = 'big string key value {}'.format(num)
+                val = 'big string value value {}'.format(num)
+                d1[key] = val
+                d2[key] = val
+            self.assertEqual(len(d1), len(d2))
+            self.assertEqual(sorted(d1.keys()), sorted(d2.keys()))
+            self.assertEqual(sorted(d1.values()), sorted(d2.values()))
+            self.assertEqual(sorted(d1.items()), sorted(d2.items()))
+
+    def test_ops(self):
+        for _try in range(15):
+            d1 = dict()
+            d2 = redblack.dict()
+            for _iter in range(5000):
+                num = random.randint(0, 10000)
+                key = 'big string key value {}'.format(num)
+                val = 'big string value value {}'.format(num)
+                op = random.randint(0,1)
+                if op == 0:
+                    d1[key] = val
+                    d2[key] = val
+                elif op == 1:
+                    d1.pop(key)
+                    d2.pop(key)
+            self.assertEqual(len(d1), len(d2))
+            self.assertEqual(sorted(d1.keys()), sorted(d2.keys()))
+            self.assertEqual(sorted(d1.values()), sorted(d2.values()))
+            self.assertEqual(sorted(d1.items()), sorted(d2.items()))
